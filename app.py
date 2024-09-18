92408c8be6540ebf24d5976d88af54be7ac841ff
Victor-K-19 <victork33@nycstudents.net>
2024-09-18 10:13:52 -0400

    Update app.py
    calc

--- app.py
+++ app.py
@@ -16,15 +16,28 @@ else:
     print("incorrect") """
     
 
-tip = input("How was your experience today? ")
+""" tip = input("How was your experience today? ")
 bill = input("What was your total price? ")
 if tip == "bad":
     print("Here is your final total:", bill)
 if tip == "ok":
-    print("Here is your final total:", (float(bill)*1.15))
+    print("Here is your final total:", round(float(bill)*1.15, 2))
 if tip == "good":
-    print("Here is your final total:", (float(bill)*1.20))
+    print("Here is your final total:", round(float(bill)*1.20, 2))
 if tip == "great":
-    print("Here is your final total:", (float(bill)*1.25))
+    print("Here is your final total with tip:", round(float(bill)*1.25, 2)) """
+
+
+""" number = int(input("Enter number: "))
+if (number % 2) == (0):
+    print((number), "is even")
+if (number % 2) == (1):
+    print((number), "is odd") """
+
+print("Enter simple equation")
+input1 = input()
+input2 = input()
+input3 = input()
+
 
 
